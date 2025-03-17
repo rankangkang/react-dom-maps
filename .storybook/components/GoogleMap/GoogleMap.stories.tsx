88c763c1c06ce84447603d0bd8647ccbf39fc459
rankangkang <rankangkang@foxmail.com>
2025-03-17 12:57:38 +0800

    feat: hook docs

--- .storybook/components/GoogleMap/GoogleMap.stories.tsx
+++ .storybook/components/GoogleMap/GoogleMap.stories.tsx
@@ -1,9 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import { GoogleMap } from '../../../src/GoogleMap'
-
-import { ExampleContainer } from './ExampleContainer'
+import { useGoogleMap, UseGoogleMapOptions } from '../../../src/useGoogleMap'
 
 const meta: Meta<typeof GoogleMap> = {
   title: 'Components/GoogleMap',
@@ -13,8 +12,41 @@ const meta: Meta<typeof GoogleMap> = {
 export default meta
 type Story = StoryObj<typeof GoogleMap>
 
-export const WithUseGoogleMap: Story = {
+export const Example: Story = {
   render() {
-    return <ExampleContainer />
+    const options = useMemo(() => {
+      const options: UseGoogleMapOptions = {
+        loader: {
+          // apiKey: YOUR_GOOGLE_MAP_API_KEY,
+          // @ts-expect-error
+          apiKey: undefined,
+        },
+        map: {
+          mapTypeId: 'roadmap',
+          center: {
+            lat: 22.3193,
+            lng: 114.1694,
+          },
+          zoom: 12,
+          zoomControl: false,
+          mapTypeControl: false,
+          scaleControl: false,
+          streetViewControl: false,
+          rotateControl: false,
+          disableDefaultUI: true,
+        },
+      }
+      return options
+    }, [])
+
+    const { api, ref } = useGoogleMap(options)
+
+    return (
+      <div className="w-full h-[600px]">
+        <GoogleMap className="w-full h-full relative" api={api} containerRef={ref}>
+          {/* any child to render */}
+        </GoogleMap>
+      </div>
+    )
   },
 }
